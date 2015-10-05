548723d0745746732fec122819f6720bf1c348bc
JoongyuSun <joongyu.sun@samsung.com>
2015-10-05 10:03:59 +0900

    orgmode default key binding

--- customizations/editing.el
+++ customizations/editing.el
@@ -89,12 +89,3 @@
 
 (global-set-key (kbd "<C-return>") 'newline-without-break-of-line)
 
-
-; org-mode
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
-(require 'org)
-;;
-;; Standard key bindings
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -3,3 +3,10 @@
 (require `org)
 (setq org-agenda-files (list "~/.deft"))
 (setq org-agenda-todo-list-sublevels nil)
+; org-mode
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+
+;; Standard key bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
