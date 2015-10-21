48476f7114a60e0a19cfcc166fc6ab9edf300b49
JoongyuSun <joongyu.sun@samsung.com>
2015-10-22 08:43:36 +0900

    add goimpots, go-eldoc and C-c C-c binds to 'compile'

--- customizations/setup-go.el
+++ customizations/setup-go.el
@@ -8,20 +8,30 @@
 ;;     (setq exec-path (split-string path-from-shell path-separator))))
 ;;(when window-system (set-exec-path-from-shell-PATH))
 (setenv "GOPATH" "/home/joongyu/workspace/gocode")
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
 (setq exec-path (cons "/usr/local/go/bin" exec-path))
 (add-to-list 'exec-path "/home/joongyu/workspace/gocode/bin")
 
 (defun my-go-mode-hook ()
+  (setq gofmt-command "goimports")
+  (go-eldoc-setup)
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq gofmt-command "goimports")
+
   ; Customize compile command to run go build
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go generate && go build -v && go test -v && go vet"))
+  (define-key (current-local-map) "\C-c\C-c" 'compile)
   ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump))
+  (local-set-key (kbd "M-]") 'godef-jump)
+  (local-set-key (kbd "M-[") 'pop-tag-mark))
+
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(local-set-key (kbd "M-]") 'godef-jump)
+(local-set-key (kbd "M-[") 'pop-tag-mark)
 
 (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 
